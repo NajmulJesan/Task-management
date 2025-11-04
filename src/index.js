7dfd7e9198564162480191db136ebe21dacb2d31
NajmulJesan <najmulislamjisan2003@gmail.com>
2025-11-04 20:27:46 +0600

    Task-2 add /health route

--- src/index.js
+++ src/index.js
@@ -18,4 +18,11 @@ app.get('/tasks', (req, res) => {
 app.listen(port, () => {
        console.log(`Server running at http://localhost:${port}`);
 });
+app.get("/health", (req, res) => {
+  res.json({
+    status: "healthy",
+    uptime: process.uptime(),
+  });
+});
+
 
